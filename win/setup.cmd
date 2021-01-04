7aaf9a3008693d4937962381c97155dc3db90170
Martin Mareš <maresmar@matfyz.cz>
2021-01-04 12:02:18 +0100

    Start shortcuts unsing (win) shortcut instead of coping

--- win/setup.cmd
+++ win/setup.cmd
@@ -1,1 +1,7 @@
-copy shortcuts.ahk "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\shortcuts.lnk" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%cd%\shortcuts.ahk" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
